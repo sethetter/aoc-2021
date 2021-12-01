2eaea3f2e1496210c77c44503a915c06b6e5d562
Seth Etter <sethetter@gmail.com>
2021-12-01 08:04:14 -0600

    Cleanup

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,18 +1,25 @@
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    let (num_depth_increases, _) = input.split("\n").fold((-1, 0), |(mut sum, prev), d_str| {
-        let d = d_str.parse::<i32>().unwrap();
-        if d > prev {
-            sum += 1;
-        }
-        (sum, d)
-    });
+    let lines: Vec<&str> = input.split("\n").into_iter().collect();
+
+    let (num_depth_increases, _) =
+        lines
+            .clone()
+            .into_iter()
+            .fold((-1, 0), |(mut sum, prev), d_str| {
+                let d = d_str.parse::<i32>().unwrap();
+                if d > prev {
+                    sum += 1;
+                }
+                (sum, d)
+            });
 
     println!("Part 1: {}", num_depth_increases);
 
-    let lines: Vec<&str> = input.split("\n").collect();
     let (num_depth_increases, _) =
-        &lines[..]
+        lines
+            .clone()
+            .as_slice()
             .windows(3)
             .fold((-1, 0), |(mut sum, prev), window| {
                 let d = window.into_iter().map(|s| s.parse::<i32>().unwrap()).sum();
