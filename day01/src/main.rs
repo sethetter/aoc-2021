d5303d1f04651f48876ee25c6c78252f61c248fe
Seth Etter <sethetter@gmail.com>
2021-12-01 06:26:45 -0600

    Day 1, part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,9 +2,25 @@ fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let (num_depth_increases, _) = input.split("\n").fold((-1, 0), |(mut sum, prev), d_str| {
         let d = d_str.parse::<i32>().unwrap();
-        if d > prev { sum += 1; }
+        if d > prev {
+            sum += 1;
+        }
         (sum, d)
     });
 
     println!("Part 1: {}", num_depth_increases);
+
+    let lines: Vec<&str> = input.split("\n").collect();
+    let (num_depth_increases, _) =
+        &lines[..]
+            .windows(3)
+            .fold((-1, 0), |(mut sum, prev), window| {
+                let d = window.into_iter().map(|s| s.parse::<i32>().unwrap()).sum();
+                if d > prev {
+                    sum += 1;
+                }
+                (sum, d)
+            });
+
+    println!("Part 2: {}", num_depth_increases);
 }
