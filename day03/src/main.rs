0d356f17f8d4c71b51241cd265d4702bbf46157a
Seth Etter <sethetter@gmail.com>
2021-12-03 10:06:54 -0600

    Day 3, part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,16 +1,48 @@
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    let (gamma, epsilon) = power_consumption(input.trim().to_owned());
+
+    let (gamma, epsilon) = gamma_and_epsilon(input.trim().split("\n").collect());
     println!("Part 1: {}", gamma * epsilon);
+
+    let ogr = filter_for_value(0, input.clone());
+    let csr = filter_for_value(1, input.clone());
+
+    println!("Part 2: {}", ogr * csr);
 }
 
-fn power_consumption(input: String) -> (u32, u32) {
-    let num_bits = input.clone().split("\n").next().unwrap().trim().len();
+fn filter_for_value(g_or_e: u8, input: String) -> u64 {
+    let num_bits = input.clone().split("n").next().unwrap().len();
+
+    let mut lines: Vec<&str> = input.split("\n").collect();
+    for b in 0..num_bits {
+        if lines.len() == 1 { break; }
+
+        let (gamma, epsilon) =  gamma_and_epsilon(lines.clone());
+        let filter_bits = match g_or_e {
+            0 => format!("{:012b}", gamma),
+            1 => format!("{:012b}", epsilon),
+            _ => unreachable!(),
+        };
+
+        lines = lines.clone().into_iter().filter(|l| {
+            l.chars().nth(b).unwrap() == filter_bits.chars().nth(b).unwrap()
+        }).collect();
+    }
+
+    if lines.len() > 1 { panic!("Filter didn't work!"); }
+
+    let str = lines.get(0).unwrap();
+    u64::from_str_radix(str, 2).unwrap()
+}
+
+fn gamma_and_epsilon(lines: Vec<&str>) -> (u32, u32) {
     let mut arr: Vec<(u32, u32)> = vec![];
+    let num_bits = lines.clone().into_iter().next().unwrap().trim().len();
+
     for _ in 0..num_bits {
         arr.push((0, 0));
     }
-    for line in input.split("\n") {
+    for line in lines.clone() {
         let bits: Vec<String> = line.split("").map(|s| s.to_owned()).collect();
         for i in 0..num_bits {
             match bits.get(i+1).unwrap().as_str() {
@@ -26,9 +58,12 @@ fn power_consumption(input: String) -> (u32, u32) {
         if arr[i].0 < arr[i].1 {
             gamma.push('1');
             epsilon.push('0');
-        } else {
+        } else if arr[i].0 > arr[i].1 {
             gamma.push('0');
             epsilon.push('1');
+        } else {
+            gamma.push('1');
+            epsilon.push('0');
         }
     }
     // combine the digits
@@ -40,5 +75,5 @@ fn power_consumption(input: String) -> (u32, u32) {
 #[test]
 fn test() {
     let sample = std::fs::read_to_string("sample.txt").unwrap();
-    assert_eq!(power_consumption(sample.to_owned()), (22, 9));
+    assert_eq!(gamma_and_epsilon(sample.split("\n").collect()), (22, 9));
 }
