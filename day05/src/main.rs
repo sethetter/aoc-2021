120b6f73d70785e994daa432eb6376e551ddfd28
Seth Etter <sethetter@gmail.com>
2021-12-06 13:56:31 -0600

    Day 5, part 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,16 +1,17 @@
 fn main() {
-    println!("Hello, world!");
-
     let input = std::fs::read_to_string("input.txt").unwrap();
 
-    let heatmap = build_heatmap(input);
+    let heatmap = build_heatmap(input.clone(), false);
     println!("Part 1: {}", count_dangerous_points(heatmap));
+
+    let heatmap = build_heatmap(input.clone(), true);
+    println!("Part 2: {}", count_dangerous_points(heatmap));
 }
 
 #[test]
 fn test_part_1() {
     let input = std::fs::read_to_string("sample.txt").unwrap();
-    assert_eq!(build_heatmap(input), vec![
+    assert_eq!(build_heatmap(input, false), vec![
         vec![0,0,0,0,0,0,0,1,0,0],
         vec![0,0,1,0,0,0,0,1,0,0],
         vec![0,0,1,0,0,0,0,1,0,0],
@@ -24,11 +25,28 @@ fn test_part_1() {
     ]);
 }
 
+#[test]
+fn test_part_2() {
+    let input = std::fs::read_to_string("sample.txt").unwrap();
+    assert_eq!(build_heatmap(input, true), vec![
+        vec![1,0,1,0,0,0,0,1,1,0],
+        vec![0,1,1,1,0,0,0,2,0,0],
+        vec![0,0,2,0,1,0,1,1,1,0],
+        vec![0,0,0,1,0,2,0,2,0,0],
+        vec![0,1,1,2,3,1,3,2,1,1],
+        vec![0,0,0,1,0,2,0,0,0,0],
+        vec![0,0,1,0,0,0,1,0,0,0],
+        vec![0,1,0,0,0,0,0,1,0,0],
+        vec![1,0,0,0,0,0,0,0,1,0],
+        vec![2,2,2,1,1,1,0,0,0,0],
+    ]);
+}
+
 type HeatMap = Vec<Vec<usize>>;
 type Point = (usize, usize);
 type Line = (Point, Point);
 
-fn build_heatmap(input: String) -> HeatMap {
+fn build_heatmap(input: String, part_2: bool) -> HeatMap {
     let parsed_coords: Vec<Line> = input.lines().map(|l| parse_line(l)).collect();
 
     let max_x = parsed_coords.clone().into_iter().flat_map(|((x1, _), (x2, _))| vec![x1, x2]).max().unwrap();
@@ -36,7 +54,7 @@ fn build_heatmap(input: String) -> HeatMap {
 
     let mut heatmap: HeatMap = vec![vec![0; max_x+1]; max_y+1];
     for line in parsed_coords {
-        heatmap = add_line_to_heatmap(heatmap, line);
+        heatmap = add_line_to_heatmap(heatmap, line, part_2);
     }
 
     heatmap
@@ -58,9 +76,11 @@ fn test_parse_line() {
     assert_eq!(parse_line("5,1 -> 9,8"), ((5, 1), (9, 8)));
 }
 
-fn add_line_to_heatmap(mut heatmap: HeatMap, ((x1, y1), (x2, y2)): Line) -> HeatMap {
-    // vertical
-    if x1 == x2 {
+fn add_line_to_heatmap(mut heatmap: HeatMap, ((x1, y1), (x2, y2)): Line, part_2: bool) -> HeatMap {
+    let vertical = x1 == x2;
+    let horizontal = y1 == y2;
+
+    if vertical {
         let mut sorted = vec![y1, y2];
         sorted.sort();
         for i in sorted[0]..=sorted[1] {
@@ -68,12 +88,29 @@ fn add_line_to_heatmap(mut heatmap: HeatMap, ((x1, y1), (x2, y2)): Line) -> Heat
         }
     }
 
-    // horizontal
-    if y1 == y2 {
+    if horizontal {
         let mut sorted = vec![x1, x2];
         sorted.sort();
         for i in sorted[0]..=sorted[1] {
             heatmap[y1][i] += 1;
+        }
+    }
+
+    if part_2 && !vertical && !horizontal {
+        let mut x = x1;
+        let mut y = y1;
+
+        loop {
+            heatmap[y][x] += 1;
+
+            if x1 < x2 && x >= x2 { break; }
+            if y1 < y2 && y >= y2 { break; }
+            if x1 > x2 && x <= x2 { break; }
+            if y1 > y2 && y <= y2 { break; }
+
+            if x1 < x2 { x += 1 } else { x -= 1};
+            if y1 < y2 { y += 1 } else { y -= 1};
+
         }
     }
 
@@ -84,13 +121,3 @@ fn count_dangerous_points(heatmap: HeatMap) -> usize {
     let points: Vec<usize> = heatmap.into_iter().flatten().filter(|x| x > &1).collect();
     points.len()
 }
-
-fn print_heatmap(heatmap: HeatMap) {
-    for y in 0..heatmap.len() {
-        println!("{}", heatmap[y].clone().into_iter().map(|x| x.to_string()).collect::<Vec<String>>().join(""));
-    }
-}
-
-// fn point_is_on_line(((x1, y1), (x2, y2)): Line, (x, y): Point) -> bool {
-//     let slope = y2-y1 / x2-x1;
-// }
