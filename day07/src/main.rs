125536d1bed66550d7786bb3fd5fa4c105a1f121
Seth Etter <sethetter@gmail.com>
2021-12-07 09:24:00 -0600

    Day 7, part 2

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,23 +1,51 @@
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    println!("Part 1: {}", best_position(input.as_str()).1);
+    println!("Part 1: {}", best_position(input.as_str(), false).1);
+    println!("Part 2: {}", best_position(input.as_str(), true).1);
 }
 
-fn best_position(input: &str) -> (usize, usize) {
-    let starts: Vec<usize> = input.split(",").map(|x| x.parse::<usize>().unwrap()).collect();
+fn best_position(input: &str, part2: bool) -> (u32, u32) {
+    let starts: Vec<u32> = input
+        .split(",")
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect();
     let min = starts.clone().into_iter().min().unwrap();
     let max = starts.clone().into_iter().max().unwrap();
-    (min..max).into_iter()
-        .map(|pos| (pos, calculate_fuel(starts.clone(), pos)))
-        .min_by(|(_, f1), (_, f2)| f1.cmp(f2)).unwrap()
+    (min..max)
+        .into_iter()
+        .map(|pos| {
+            let fuel = if part2 {
+                calculate_fuel2(starts.clone(), pos)
+            } else {
+                calculate_fuel1(starts.clone(), pos)
+            };
+            (pos, fuel)
+        })
+        .min_by(|(_, f1), (_, f2)| f1.cmp(f2))
+        .unwrap()
 }
 
-fn calculate_fuel(starts: Vec<usize>, pos: usize) -> usize {
-    starts.iter().map(|s| if s > &pos { s - pos } else { pos - s }).sum()
+fn calculate_fuel1(starts: Vec<u32>, pos: u32) -> u32 {
+    starts
+        .iter()
+        .map(|s| if s > &pos { s - pos } else { pos - s })
+        .sum()
+}
+
+fn calculate_fuel2(starts: Vec<u32>, pos: u32) -> u32 {
+    starts
+        .iter()
+        .map(|s| {
+            let steps = if s > &pos { s - pos } else { pos - s };
+            ((f64::from(steps) / 2.0) * f64::from(1 + steps)) as u32
+        })
+        .sum()
 }
 
 #[test]
-fn test_part1() {
+fn test_part() {
+    println!(" 5 / 2 = {}", 5 / 2);
     let input = "16,1,2,0,4,2,7,1,2,14";
-    assert_eq!(best_position(input).0, 2);
+    assert_eq!(best_position(input, false), (2, 37));
+    assert_eq!(best_position(input, true), (5, 168));
 }
